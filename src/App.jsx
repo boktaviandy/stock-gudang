99c7d7c5d28fa293569a41bfe5c3cc5fc616a826
Bisma Oktaviandy <68682374+boktaviandy@users.noreply.github.com>
2026-07-27 02:10:16 +0700

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppRouter } from './router/AppRouter';
+import './styles/index.css';
+import './styles/animations.css';
+
+export function App() {
+  return <AppRouter />;
+}
+
+export default App;
